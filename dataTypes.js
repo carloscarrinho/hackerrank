4fe7c546f8de72ef76acc699ee3cb3924590a77b
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2020-04-09 18:03:40 -0300

    Data Types challenge on HackerRank

---
+++ dataTypes.js
@@ -0,0 +1,11 @@
+var firstInteger = 10;
+var firstDecimal = 0.5;
+var firstString = "Carlos";
+
+var secondInteger = "20";
+var secondDecimal = "2.5";
+var secondString = "Eduardo";
+
+console.log(firstInteger + parseInt(secondInteger));
+console.log(firstDecimal + parseFloat(secondDecimal));
+console.log(firstString.concat(" ", secondString));
